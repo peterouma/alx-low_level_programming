01eaeaea0e847386a3465c8da77f3f70eae5d50a
peterouma <peteouma24@gmail.com>
2023-03-28 04:56:40 -0700

    hello world tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main function entry point
+ * Description: prints the specified string
+ * Return: 0
+ */
+int main(void)
+{
+	char a[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf("%s", a);
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * Description: print size of data type
+ * Return: 0
+ */
+
+int main(void)
+{
+	int charSize = sizeof(char);
+	int intSize = sizeof(int);
+	int longIntSize = sizeof(long int);
+	int longLongIntSize = sizeof(long long int);
+	int floatSize = sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", charSize);
+	printf("Size of an int: %d byte(s)\n", intSize);
+	printf("Size of a long int: %d byte(s)\n", longIntSize);
+	printf("Size of a long long int: %d byte(s)\n", longLongIntSize);
+	printf("Size of a float: %d byte(s)\n", floatSize);
+	return (0);
+}
