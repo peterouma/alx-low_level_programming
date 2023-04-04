ce3e5f9a1f9431320ed8c217c37b8943c2700080
peterouma <peteouma24@gmail.com>
2023-04-04 12:56:09 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  *_isalpha - checks for alphabets
+  *@c: character is a letter,0 otherwise
+  */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
+
